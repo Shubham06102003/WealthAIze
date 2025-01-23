eceec9e5c8fd1d8e078a58fef8a3da43c88b55d9
Shubham06102003 <shubhamudgirkar2025@gmail.com>
2025-01-24 01:44:40 +0530

    final commit

--- components/hero.jsx
+++ components/hero.jsx
@@ -1,14 +1,16 @@
-"use client";
-import Link from "next/link";
+"use client"
+
 import React, { useEffect, useRef } from "react";
-import { Button } from "./ui/button";
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const HeroSection = () => {
-  const imageRef = useRef();
+  const imageRef = useRef(null);
 
   useEffect(() => {
     const imageElement = imageRef.current;
+
     const handleScroll = () => {
       const scrollPosition = window.scrollY;
       const scrollThreshold = 100;
@@ -19,19 +21,20 @@ const HeroSection = () => {
         imageElement.classList.remove("scrolled");
       }
     };
+
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
   return (
-    <div className="pb-20 px-4">
+    <section className="pb-20 px-4">
       <div className="container mx-auto text-center">
         <h1 className="text-5xl md:text-8xl lg:text-[105px] pb-6 gradient-title">
-          Manage your Finances <br /> with Intelligence
+          Manage Your Finances <br /> with Intelligence
         </h1>
         <p className="text-xl text-gray-600 mb-8 max-w-2xl mx-auto">
           An AI-powered financial management platform that helps you track,
-          analyze, and optimze your spendings with real-time insights.
+          analyze, and optimize your spending with real-time insights.
         </p>
         <div className="flex justify-center space-x-4">
           <Link href="/dashboard">
@@ -40,7 +43,7 @@ const HeroSection = () => {
             </Button>
           </Link>
           <Link href="">
-            <Button size="lg" variant="outlined" className="px-8">
+            <Button size="lg" variant="outline" className="px-8">
               Watch Demo
             </Button>
           </Link>
@@ -51,14 +54,14 @@ const HeroSection = () => {
               src="/banner.jpg"
               width={1280}
               height={720}
-              alt="Dashboard preview"
+              alt="Dashboard Preview"
               className="rounded-lg shadow-2xl border mx-auto"
               priority
             />
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
